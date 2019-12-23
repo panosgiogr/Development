1f6ede73b9663d53cd746290dc161f5300f4513a
panosgiogr <46088894+panosgiogr@users.noreply.github.com>
2019-12-23 13:30:49 +0200

    up

--- Holidays/ΑΣΚ 8.py
+++ Holidays/ΑΣΚ 8.py
@@ -1,13 +1,12 @@
 number=int(input("Δώσε έναν αριθμό : "))
-num=number
-c=0
-while num>0:
-  num=num/10
-  c+=1
-new_num=0
-for i in range(c):
-    new_num=(num/(10**c-1))
-    num-=new_num*(10**c-1)
-    c-=1
-    print new_num
-    
+di = 0
+for i in range(number):
+    di=di+number%10
+    number=number/10
+    print di
+    print number
+if di==9:
+  print "Ο αριθμός διαιρείται με το 9"
+else:
+  print "Ο αριθμός δεν διαιρείται με το 9"
+
